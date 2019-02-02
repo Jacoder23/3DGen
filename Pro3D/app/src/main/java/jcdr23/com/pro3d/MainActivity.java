f4021588625d76d99bfe1379a95de050ea4fea5b
Jacoder23 <Jacoder23@users.noreply.github.com>
2019-02-02 19:58:58 +0800

    Added Pro3D 0.0.0

---
+++ Pro3D/app/src/main/java/jcdr23/com/pro3d/MainActivity.java
@@ -0,0 +1,13 @@
+package jcdr23.com.pro3d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
