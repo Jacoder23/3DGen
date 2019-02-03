64d8356769c9f3427c14ca35b16a6187f730f37c
Jacoder23 <Jacoder23@users.noreply.github.com>
2019-02-03 17:40:41 +0800

    Added button click listeners

--- Pro3D/app/src/main/java/jcdr23/com/pro3d/MainActivity.java
+++ Pro3D/app/src/main/java/jcdr23/com/pro3d/MainActivity.java
@@ -2,7 +2,8 @@ package jcdr23.com.pro3d;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bool folderSelected = false;
+
+        Button selectFolder = findViewById(R.id.btn_selectFolder);
+        selectFolder.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Make this onClick listener invoke a call to our file selection library to select a folder
+                // TODO Make it so when the activity this button invokes finishes and there is no folder selected, an alert pops up
+            }
+        });
+
+        Button start = findViewById(R.id.btn_selectFolder);
+        selectFolder.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Make this start another activity where the earlier files will be processed.
+                // TODO Make it so when this button is clicked and there is no folder selected, an alert pops up
+            }
+        });
     }
 
 
