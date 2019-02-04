bd9a068e547361bed646d6821cbfc92c363bda11
Jacoder23 <Jacoder23@users.noreply.github.com>
2019-02-04 13:01:45 +0800

    File access and small alerts have been added.

--- Pro3D/app/src/main/java/jcdr23/com/pro3d/MainActivity.java
+++ Pro3D/app/src/main/java/jcdr23/com/pro3d/MainActivity.java
@@ -1,9 +1,37 @@
+// If you are ever feeling angry or down, just remember fricatta. - Albert
+
 package jcdr23.com.pro3d;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import java.io.File;
+import android.util.Log;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.view.MenuItem;
+import android.app.Dialog;
+
+import com.github.angads25.filepicker.controller.DialogSelectionListener;
+import com.github.angads25.filepicker.model.DialogConfigs;
+import com.github.angads25.filepicker.model.DialogProperties;
+import com.github.angads25.filepicker.view.FilePickerDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,19 +45,56 @@ public class MainActivity extends AppCompatActivity {
         Button selectFolder = findViewById(R.id.btn_selectFolder);
         selectFolder.setOnClickListener( new View.OnClickListener() {
             public void onClick(View v) {
-                // TODO Make this onClick listener invoke a call to our file selection library to select a folder
-                // TODO Make it so when the activity this button invokes finishes and there is no folder selected, an alert pops up
+
+                DialogProperties properties = new DialogProperties();
+
+                properties.selection_mode = DialogConfigs.SINGLE_MODE;
+                properties.selection_type = DialogConfigs.DIR_SELECT;
+                properties.root = new File(DialogConfigs.DEFAULT_DIR);
+                properties.error_dir = new File(DialogConfigs.DEFAULT_DIR);
+                properties.offset = new File(DialogConfigs.DEFAULT_DIR);
+                properties.extensions = null;
+
+                FilePickerDialog dialog = new FilePickerDialog(MainActivity.this,properties);
+                dialog.setTitle("Select your images");
+
+                dialog.setDialogSelectionListener(new DialogSelectionListener() {
+                    @Override
+                    public void onSelectedFilePaths(String[] files) {
+                        //files is the array of the paths of files selected by the Application User.
+                    }
+                });
+
+                dialog.show();
+
+                Log.i("fricatta", "btn_selectFolder clicked!");
             }
         });
 
-        Button start = findViewById(R.id.btn_selectFolder);
-        selectFolder.setOnClickListener( new View.OnClickListener() {
+        Button start = findViewById(R.id.btn_start);
+        start.setOnClickListener( new View.OnClickListener() {
             public void onClick(View v) {
                 // TODO Make this start another activity where the earlier files will be processed.
                 // TODO Make it so when this button is clicked and there is no folder selected, an alert pops up
+
+                Log.i("fricatta", "btn_start clicked!");
             }
         });
     }
 
+    public void onRequestPermissionsResult(int requestCode,@NonNull String permissions[],@NonNull int[] grantResults) {
+        switch (requestCode) {
+            case FilePickerDialog.EXTERNAL_READ_PERMISSION_GRANT: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission has been granted.
+                    Toast.makeText(MainActivity.this,"Thank you. Please continue.",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //Permission has not been granted. Notify the user.
+                    Toast.makeText(MainActivity.this,"We need permission to access your dataset.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
 }
